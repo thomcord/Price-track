d845ef819ab50a347fb22c5be4c0c1289baf7517
Thomas Cordeiro <thomcord@gmail.com>
2021-06-08 14:02:39 +0200

    complete code

--- scraper.py
+++ scraper.py
@@ -16,12 +16,37 @@ def price():
     price = soup.find(id="priceblock_ourprice").get_text()
     converted_price = float(price.replace(" €", "").replace(",", "."))
 
+    if(converted_price < 140):
+        send_email()
+
   
 
     print(converted_price)
     print(title.strip())
+# Send email function
 
+def send_email():
+    server = SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
+# Password must be added
 
+    server.login('thomcord@gmail.com', 'PASSWORD')
+
+    subject = "Price fell down"
+    body = "Check the link https://www.amazon.de/Dell-S2421H-Zoll-1920x1080-entspiegelt/dp/B08GGDHQ99/ref=sr_1_12?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=monitor&qid=1622205502&sr=8-12"
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        'thomcord@gmail.com',
+        'thomcord@me.com',
+        msg
+    )
+    print('Email has been sent')
+
+    server.quit()
 
 price()
